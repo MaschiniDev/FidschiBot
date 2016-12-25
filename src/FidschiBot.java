e7447db5a7b97ce709e13fa61475abb3c5b90644
Maschini <maschinigames@gmail.com>
2016-12-25 20:14:07 +0100

    Initial Project Commit

---
+++ src/FidschiBot.java
@@ -0,0 +1,9 @@
+import org.jibble.pircbot.PircBot;
+
+public class FidschiBot extends PircBot {
+    public FidschiBot()
+    {
+        //System.out.println("Hello World!");
+        this.setName("DerFidschi");
+    }
+}
