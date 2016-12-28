75ecf4b00ecd05abbd83e8d2fbcabac925813d6f
Maschini <maschinigames@gmail.com>
2016-12-28 23:02:24 +0100

    new main

--- src/FidschiBot.java
+++ src/FidschiBot.java
@@ -1,13 +1,18 @@
 import org.jibble.pircbot.PircBot;
 
 public class FidschiBot extends PircBot {
-    public FidschiBot() {
+    public FidschiBot() throws Exception {
         this.setName("derfidschi");
+        this.setVerbose(true);
+        this.connect("irc.chat.twitch.tv", 6667, "oauth:wina1jfkwzqzoraqb0vqy7jes8m93l");
     }
 
     public void onMessage(String channel, String sender, String login, String hostname, String message) {
-        if(message.equalsIgnoreCase("!ts")) {
-            System.out.println("lg.de");
+        if(message.equalsIgnoreCase("!ts") || message.equalsIgnoreCase("!ts3") || message.equalsIgnoreCase("!teamspeak")) {
+            sendMessage(channel, sender + " -> legendarygamer.de");
+        }
+        if(message.equalsIgnoreCase("!test")) {
+            sendMessage(channel, "test");
         }
     }
 }
--- src/main.java
+++ src/main.java
@@ -4,16 +4,20 @@ import java.io.InputStreamReader;
 public class main {
     public static void main(String[] args) throws Exception {
 
+        //#buffreader
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+        /*
+        *   Start PircBot
+        *   (c) Paul Mutton 2001-2013
+        *   #newbot
+        */
         System.out.println("Channel?");
         String channel = "#" + br.readLine();
 
+        //define bot
         FidschiBot bot = new FidschiBot();
-        bot.setVerbose(true);
-        bot.connect("irc.chat.twitch.tv", 6667, "oauth:wina1jfkwzqzoraqb0vqy7jes8m93l");
         bot.joinChannel(channel);
-
-        bot.sendMessage(channel, "Der Fidschi is in da House [BOT ACTIVATED]");
+        //bot.sendMessage(channel, "[BOT ACTIVATED]");
     }
 }
